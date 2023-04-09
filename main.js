93000a5d2c469ae033ef8161d31ca03d50f20edf
xai169 <rulenkovstepan@gmail.com>
2023-04-09 19:20:17 +0300

    09.04
    Начал делать js

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-let popup = document.querySelector('.popup-container');
-let popupBtn = document.querySelector('.contacts-btn');
-let crossBtn = document.querySelector('.cross-form');
+const popup = document.querySelector('.popup-container');
+const popupBtn = document.querySelector('.contacts-btn');
+const crossBtn = popup.querySelector('.cross-form');
+const firstName = popup.querySelector('[name=name]');
+const email = popup.querySelector('[name=email]');
+const offer = popup.querySelector('[name=offer]');
+const form = popup.querySelector('form');
+const nameStorage = localStorage.getItem('name', firstName.value);
+const emailStorage = localStorage.getItem('email', email.value);
 
-popupBtn.onclick  = function() {
+popupBtn.addEventListener('click', function() {
     popup.classList.remove('disp-none');
-    console.log('KNOPKA');
-}
+    firstName.focus();
+    if (nameStorage && emailStorage) {
+        firstName.value = nameStorage;
+        email.value = emailStorage;
+        offer.focus();
+    }
+});
 
-crossBtn.onclick  = function() {
+crossBtn.addEventListener('click', function() {
     popup.classList.add('disp-none');
-    console.log('KNOPKA');
-}
+});
+
+document.addEventListener('keydown', function(evt) {
+    if (evt.key === 'Escape') {
+        popup.classList.add('disp-none'); 
+    }
+});
+
+form.addEventListener('submit', function(evt) {
+    if (!firstName.value || !email.value || !offer.value) {
+    evt.preventDefault();
+    } else {
+        localStorage.setItem('name', firstName.value);
+        localStorage.setItem('email', email.value);
+    }
+});
---
+++ slider.js
@@ -0,0 +1,4 @@
+const slide1 = document.querySelector('.main-slider-wrapper .bg-phone');
+const button = document.querySelector('.main-slider-wrapper .switch-btn:nth-child(1)');
+console.log(slide)
+console.log(button)
